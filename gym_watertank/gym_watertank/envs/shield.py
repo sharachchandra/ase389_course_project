241cd81a734be9f4940ad9c673dbf3bafa3e3ef9
Sai Shankar <sai.nshankar@gmail.com>
2022-05-13 12:15:52 -0500

    aded

--- gym_watertank/gym_watertank/envs/shield.py
+++ gym_watertank/gym_watertank/envs/shield.py
@@ -1,12 +1,14 @@
-class Shield():
+class Shield:
 
     def __init__(self):
         print("Initializing the real shield")
         print("The shielded actions function returns the set of safe actions in a state")
 
+        #self.
+
 
     def shielded_actions(self, state_val, switch_state):
-        # always returns a list
+        #print('always returns a list')
         if 1 <= state_val <= 3 and switch_state == 1: 
             return [1]
         elif 97 <= state_val <= 99 and switch_state == -1:
---
+++ scripts/.ipynb_checkpoints/main_watertank-checkpoint.py
@@ -0,0 +1,5 @@
+import gym
+import gym_watertank
+
+env = gym.make('watertank-v0', use_shield=False)
+
